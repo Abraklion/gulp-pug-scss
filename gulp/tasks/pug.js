55cc9a2c76733a9c03cd4caec52265fc7a0fb818
Abraklion <abraklion@gmail.com>
2021-04-20 13:57:44 +0500

    Сборка Gulp PUG SCSS v1.0.0

---
+++ gulp/tasks/pug.js
@@ -0,0 +1,23 @@
+// Компелирует Pug файлы в Html и переносит файлы в папку dist
+
+module.exports = function () {
+
+  $.gulp.task('pug', () => {
+
+    return $.gulp.src($.config.paths.pug, { base: "src/" })
+      .pipe($.gp.plumber())
+      .pipe($.gp.pug({
+        locals: {
+            jsonData: JSON.parse($.fs.readFileSync('./src/templates/data/data.json', 'utf8'))
+        }
+      }))
+      .pipe($.gp.htmlBeautify({
+        "indent_size": 4,
+        "max_preserve_newlines": 1
+      }))
+      .pipe($.gp.if($.config.toggle.fullHtml, $.gp.htmlmin({ collapseWhitespace: true })))
+      .pipe($.gulp.dest($.config.output.path))
+      .pipe($.browserSync.stream());
+  });
+}
+
---
+++ gulp/tasks/watcher.js
@@ -0,0 +1,16 @@
+// Открывает слежку за файлами
+
+module.exports = function () {
+
+  $.gulp.task('watcher', (done) => {
+
+    $.gulp.watch([$.config.watch.pug, 'src/templates/data/*.json', 'src/templates/helpers/*.md'], $.gulp.series("pug"));
+    $.gulp.watch($.config.watch.css, $.gulp.series("styles"));
+    $.gulp.watch($.config.watch.js, $.gulp.series("scripts"));
+    $.gulp.watch($.config.watch.fonts, $.gulp.series("fonts"));
+    $.gulp.watch([$.config.watch.images.img, $.config.watch.images.webp], $.gulp.series("images"));
+    $.gulp.watch($.config.watch.images.svg, $.gulp.series("sprite"));
+
+    done();
+  });
+}
